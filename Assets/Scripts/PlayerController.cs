8ae433de7763ebe7c520263b610fc727acb0182f
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-10-13 20:40:42 +0200

    Added movement to ship.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		Rigidbody rigibody = GetComponent<Rigidbody>();
+		rigibody.velocity = movement * speed;
+
+		rigibody.position = new Vector3 
+		(
+				Mathf.Clamp (rigibody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+				Mathf.Clamp (rigibody.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rigibody.rotation = Quaternion.Euler (0.0f, 0.0f, rigibody.velocity.x * -tilt);
+	}
+}
