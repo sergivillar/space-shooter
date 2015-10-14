bd19fe79f97ec33f77d6fd1ded665173b9da36e3
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-10-14 21:41:36 +0200

    Added shots to ship.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,47 @@
 [System.Serializable]
 public class Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float speed;
-	public float tilt;
-	public Boundary boundary;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            // GameObject clone =
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+        }
+    }
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Rigidbody rigibody = GetComponent<Rigidbody>();
-		rigibody.velocity = movement * speed;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		rigibody.position = new Vector3 
-		(
-				Mathf.Clamp (rigibody.position.x, boundary.xMin, boundary.xMax),
-			0.0f,
-				Mathf.Clamp (rigibody.position.z, boundary.zMin, boundary.zMax)
-		);
+        Rigidbody rigibody = GetComponent<Rigidbody>();
+        rigibody.velocity = movement * speed;
 
-		rigibody.rotation = Quaternion.Euler (0.0f, 0.0f, rigibody.velocity.x * -tilt);
-	}
+        rigibody.position = new Vector3
+        (
+                Mathf.Clamp(rigibody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+                Mathf.Clamp(rigibody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigibody.rotation = Quaternion.Euler(0.0f, 0.0f, rigibody.velocity.x * -tilt);
+    }
 }
