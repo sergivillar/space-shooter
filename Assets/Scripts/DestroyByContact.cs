07b3afad828aff415a177d3d318f55df987932d6
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-10-15 21:06:30 +0200

    Added rotate asteroid and destroy it.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+		if (other.tag == "Boundary")
+			return;
+			
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    void Start()
+    {
+        Rigidbody rigibody = GetComponent<Rigidbody>();
+        rigibody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
