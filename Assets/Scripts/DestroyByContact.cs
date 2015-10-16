2506730bbd7f8081e42ee48eecbadac37503b735
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-10-16 09:24:22 +0200

    Added explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,19 @@
 public class DestroyByContact : MonoBehaviour
 {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     void OnTriggerEnter(Collider other)
     {
-		if (other.tag == "Boundary")
-			return;
-			
-		Destroy(other.gameObject);
-		Destroy(gameObject);
+        if (other.tag == "Boundary")
+            return;
+		
+        if (other.tag == "Player")
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
     }
 }
