ff4d68c497465ca2017e386e110e587437bfc3e9
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-10-18 13:49:21 +0200

    Added background scroll.

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+	
+	private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+		startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPostion = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPostion;
+    }
+}
